6b088d33293c7cd14ff2510d35ea63b034101eea
lafamiliaramirez723-hub <lafamiliaramirez723@gmail.com>
2026-03-17 22:40:10 -0400

    Add Java program to count even numbers

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("¿Cuántos números?: ");
+        int n = sc.nextInt();
+
+        int pares = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Número: ");
+            int num = sc.nextInt();
+
+            if (num % 2 == 0) {
+                pares++;
+            }
+        }
+
+        System.out.println("Pares: " + pares);
+    }
+}
